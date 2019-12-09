cc37d825fe81de07785807e52381520b61f9c7e5
Takashi@Mac <hamada@gift10.co.jp>
2019-12-09 20:52:56 +0900

    ゲーム選択メニューの作成　バージョン更新

---
+++ Assets/Common/Scripts/GameSelectionManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameSelectionManager : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.Instance.PlaySe("select-guide");
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("MultiLines_0");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("ShingleLine_Misawa");
+        }
+    }
+}
