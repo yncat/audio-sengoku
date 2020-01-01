0b000356056a2c8a10141436f6b40b069991f630
misawa_macbook <misawa_makbook@misawa-macbooknoMacBook.local>
2020-01-01 19:46:50 +0900

    updated tick timer

---
+++ Assets/TurnBase_Misawa/Scripts/TickTimer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TurnM
+{
+    public class TickTimer : MonoBehaviour
+    {
+        public bool isActive = false;
+        [SerializeField, ReadOnlyWhenPlayingAttribute, Range(1f, 30f)] float m_tickTime = 10f;
+        [SerializeField, ReadOnlyWhenPlayingAttribute] Slider m_slider = null;
+        [SerializeField, ReadOnly] bool m_isTick = false;
+        [SerializeField] AudioSource m_tickAc = null;
+        public bool isTick { get { return m_isTick; } }
+        public bool isOdd { get { return ((m_tickCount >= 0) && ((m_tickCount & 1) == 1)); } }
+        public bool isEven { get { return ((m_tickCount >= 0) && ((m_tickCount & 1) == 0)); } }
+        float m_timer;
+        int m_tickCount=-1;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_isTick = false;
+            m_timer = m_tickTime;
+            m_tickCount = -1;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            m_isTick = false;
+            if (isActive)
+            {
+                m_timer -= Time.deltaTime;
+                if (m_timer <= 0f)
+                {
+                    m_isTick = true;
+                    m_timer += m_tickTime;
+                    m_tickCount++;
+                    if (m_tickAc != null)
+                    {
+                        m_tickAc.Play();
+                        if (m_slider != null)
+                        {
+                            ColorBlock cblk = m_slider.colors;
+                            cblk.normalColor = (isOdd ? Color.blue : Color.red);
+                            m_slider.colors = cblk;
+                        }
+                    }
+                }
+                if (m_slider != null)
+                {
+                    m_slider.value = 1f-(m_timer / m_tickTime);
+                }
+            }
+            else
+            {
+                m_tickCount = -1;
+            }
+
+        }
+    }
+}
