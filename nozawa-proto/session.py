2f7fc74c344befaabad97982a6cf609f6f85d7d6
nyanchan <personal@nyanchangames.com>
2020-01-12 12:07:33 +0900

    Still buggy, but game

--- nozawa-proto/session.py
+++ nozawa-proto/session.py
@@ -22,12 +22,14 @@ class Session(object):
 			self.field.append([None]*constants.FIELD_SIZE_Y)
 		self.turn=1
 		self.current_player=0
-		self.units=[]
 
 	def start(self):
-		globalVars.app.playSound("general/Jingle_Start.ogg")
+		win=None
 		while(True):
 			p=self.players[self.current_player]
+			win=self.moveUnits(p)
+			if win: break
+			globalVars.app.playSound("general/Jingle_Start.ogg")
 			globalVars.app.say("%sの先述" % p.name)
 			lane=p.getLane()
 			if lane==-1: break
@@ -40,7 +42,40 @@ class Session(object):
 			self.current_player+=1
 			if self.current_player==2: self.advanceTurn()
 		#end while
+		if win:
+			app.playSound("general/Jingle_End_Win.ogg")
+			app.wait(2500)
+			app.say("%sの勝利!" % win.name)
+		#end 勝利
 	#end start
+
+	def moveUnits(self,p):
+		moved_units=[]#同じユニットを2回動かさないように
+		win=None
+		for i in range(constants.FIELD_SIZE_X):
+			for j in range(constants.FIELD_SIZE_Y):
+				u=self.field[i][j]
+				if u is None: continue
+				if u.owner is not p: continue#そのターンのプレイヤーの兵だけ動かす
+				if u in moved_units: continue#すでに動かした
+				moved_units.append(u)
+				newpos=j+1 if u.owner.direction==constants.DIRECTION_UP else j-1
+				u2=self.field[i][newpos]
+				if u2 is None:
+					self.field[i][newpos]=u
+					self.field[i][j]=None
+					if newpos==0 or newpos==constants.FIELD_SIZE_Y:
+						if newpos!=u.owner.y: win=u.owner
+					#end プレイヤーの勝利判定
+				else:
+					self.field[i][j]=None
+					self.field[i][newpos]=None
+					globalVars.app.playSound("general/ashigaru_combat.ogg")
+					globalVars.app.wait(3000)
+				#end ユニットの勝敗
+			#end フィールドスキャン
+		#end フィールドスキャン
+		return win
 
 	def sendMessenger(self,p,lane):
 		globalVars.app.playSound("general/messenger.ogg")
@@ -70,7 +105,6 @@ class Session(object):
 
 	def sendUnit(self,p,lane):
 		u=units.Ashigaru(p)
-		self.units.append(u)
 		self.field[lane][p.y]=u
 		u.playSpawnSound()
 
